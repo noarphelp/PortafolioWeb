9cce53edd52c8d8140be5ab2bc61bb48c812dd0a
noarphelp <ordestrader@gmail.com>
2025-10-03 23:37:32 +0200

    Corregido set-env

--- set-env.js
+++ set-env.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 
+console.log("🔍 Variables de entorno leídas por Netlify:");
+console.log("EMAIL_SERVICE_ID:", process.env.EMAIL_SERVICE_ID);
+console.log("EMAIL_TEMPLATE_ID:", process.env.EMAIL_TEMPLATE_ID);
+console.log("EMAIL_PUBLIC_KEY:", process.env.EMAIL_PUBLIC_KEY);
+
 const envConfigFile = `export const environment = {
   production: true,
   emailServiceId: "${process.env.EMAIL_SERVICE_ID}",
@@ -10,3 +15,4 @@ const envConfigFile = `export const environment = {
 
 fs.writeFileSync('./src/environments/environment.prod.ts', envConfigFile);
 console.log('✅ environment.prod.ts generado con variables de Netlify');
+
