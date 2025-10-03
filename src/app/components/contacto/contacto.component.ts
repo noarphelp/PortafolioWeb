1bae6a271e7f22d55c1553c0a989dec49046a1ac
noarphelp <ordestrader@gmail.com>
2025-10-04 01:47:12 +0200

    Corregido credenciales4

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import emailjs from '@emailjs/browser';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { environment } from '../../../environments/environment.prod';
 
 @Component({
